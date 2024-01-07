aae9ea9faa28b54a399764d680988e295b6aad74
Monil Karia <monilkaria2019@gmail.com>
2024-01-07 12:46:09 +0530

    JS Practice and MDN-Maths Practice added

---
+++ JS-Practice/variables.js
@@ -0,0 +1,13 @@
+//Variables in JS
+let admin;
+let name1;
+name1="John";
+admin = name1;
+console.log(admin);
+
+// Naming in the JS
+let currPlanetName="Earth";
+let currUserName = "Monil";
+
+/* IF we know the variable is HARD-CODED then use Captail
+and if it is going to change somewhere in future then Normal TEXT */
