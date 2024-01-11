290f61cab774cab282020510eba2e402c7c9b7aa
Monil Karia <monilkaria2019@gmail.com>
2024-01-11 11:38:36 +0530

    RPS-Game Logic updated with

--- rock-paper-scissor/script.js
+++ rock-paper-scissor/script.js
@@ -1,25 +1,40 @@
-// Main Logic 
-function getComputerChoice(){
-    let randomChoice = Math.floor(Math.random() * 100) % user + 1;
-    let computerChoice;
-    // console.log(choice);
-    computerChoice = (randomChoice === 1) ? 'Rock' : (randomChoice == 2) ? 'Paper' : 'Scissors';
-    // battleWinner(computerChoice , computerChoice);
+function getComputerChoice() {
+    let randomChoice = Math.floor(Math.random() * 100) % 3 + 1;
+    return (randomChoice === 1) ? 'Rock' : (randomChoice === 2) ? 'Paper' : 'Scissors';
 }
-function battleWinner(){
-    let computerScore  = 0;
+
+function getUserChoice() {
+    let userChoice;
+    do {
+        userChoice = prompt('Choose among these: \n 1. Rock \n 2. Paper \n 3. Scissors');
+    } while (![1, 2, 3].includes(parseInt(userChoice)));
+
+    return userChoice;
+}
+
+function battleWinner() {
+    let computerScore = 0;
     let userScore = 0;
-    while(computerScore < 5 && userScore < 5){
+
+    while (computerScore < 5 && userScore < 5) {
         let computerChoice = getComputerChoice();
-        let userChoice = prompt('Choice among thse \n 1. Rock \n 2. Paper \n 3. Scissors');
-        if((computerChoice == 'Rock' && userChoice == 'Paper') || (computerChoice == 'Paper' && userChoice == 'Scissors') || (computerChoice == 'Scissors' && userChoice == 'Rock')){
+        let userChoice = getUserChoice();
+
+        if ((computerChoice === 'Rock' && userChoice === '2') ||
+            (computerChoice === 'Paper' && userChoice === '3') ||
+            (computerChoice === 'Scissors' && userChoice === '1')) {
             userScore++;
-            console.log(`User Score: ${userScore}`);
-        }else if((userChoice == 'Rock' && computerChoice == 'Paper') || (userChoice == 'Paper' && computerChoice == 'Scissors') || (userChoice == 'Scissors' && computerChoice == 'Rock')){
+            console.log(`userScore: ${userScore}`);
+        } else if ((userChoice === '1' && computerChoice === 'Paper') ||
+            (userChoice === '2' && computerChoice === 'Scissors') ||
+            (userChoice === '3' && computerChoice === 'Rock')) {
             computerScore++;
-            console.log(`Computer Score: ${computerScore}`);
+            console.log(`computerScore: ${computerScore}`);
         }
     }
-    if(computerScore == 5) console.log("Sadly Computer is More Smater than you");
-    else if(userScore== 5) console.log("Yeahh.! AI will not take ur Job");
+
+    if (computerScore === 5) console.log("Sadly, the computer is smarter than you");
+    else if (userScore === 5) console.log("Yeahh.! AI will not take your job");
 }
+
+battleWinner();
